3c79921adc225edd05d597cd4ce77cc3313d48a9
Subashree <subashree.25cs@licet.ac.in>
2025-06-05 10:59:00 +0530

    initial commit

---
+++ src/main/java/com/example/helloapp/HelloappApplication.java
@@ -0,0 +1,18 @@
+package com.example.helloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class HelloappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloappApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello from Subashree!";
+	}
+}
